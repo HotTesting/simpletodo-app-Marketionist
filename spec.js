c0f0df933de67625b7fc9094698a5bae668463a6
Marketionist <marketionist@gmail.com>
2016-11-13 20:48:16 +0200

    Made all tests independant + minor refactoring

--- spec.js
+++ spec.js
@@ -4,6 +4,16 @@ let URL = 'http://maxcabrera.com/code/todo-list/';
 let notes = $$('todo-list .small-12');
 let textNewTask = 'Q11';
 
+function createNewNote() {
+    let inputNewTask = $('input.enter-todo');
+
+    browser.get(URL);
+    inputNewTask.sendKeys(textNewTask);
+    inputNewTask.submit();
+
+    expect(notes.getText()).toContain(textNewTask);
+}
+
 describe('TODO site', function () {
 
     it('should have correct title', function () {
@@ -12,19 +22,14 @@ describe('TODO site', function () {
     });
 
     it('should create new note', function () {
-        let inputNewTask = $('input.enter-todo');
-
-        browser.get(URL);
-        inputNewTask.sendKeys(textNewTask);
-        inputNewTask.submit();
-
-        expect(notes.getText()).toContain(textNewTask);
+        createNewNote();
     });
 
     it('should delete new note', function () {
         let noteNew = element(by.xpath('//*[ancestor::*[@class="todo-container"' +
             ' and descendant::*[text()="' + textNewTask + '"]] and @type="checkbox"]'));
 
+        createNewNote();
         noteNew.click();
 
         expect(notes.getText()).not.toContain('Q11');
@@ -33,6 +38,7 @@ describe('TODO site', function () {
     it('should delete all notes', function () {
         let notesAll = element.all(by.css('.todo-container [type="checkbox"]'));
 
+        browser.get(URL);
         notesAll.click();
 
         expect(notesAll.count()).toBe(0);
