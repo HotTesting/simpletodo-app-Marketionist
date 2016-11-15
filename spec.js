cce9947878eacf8224e3fa64e2d5d495d119054f
Marketionist <marketionist@gmail.com>
2016-11-15 13:55:47 +0200

    Added beforeEach function usage

--- spec.js
+++ spec.js
@@ -1,13 +1,12 @@
 //JasmineJS 2
 
 let URL = 'http://maxcabrera.com/code/todo-list/';
-let notes = $$('todo-list .small-12');
+let notes = element.all(by.css('todo-list .small-12'));
 let textNewTask = 'Q11';
 
 function createNewNote() {
     let inputNewTask = $('input.enter-todo');
 
-    browser.get(URL);
     inputNewTask.sendKeys(textNewTask);
     inputNewTask.submit();
 
@@ -16,8 +15,11 @@ function createNewNote() {
 
 describe('TODO site', function () {
 
-    it('should have correct title', function () {
+    beforeEach(function () {
         browser.get(URL);
+    });
+
+    it('should have correct title', function () {
         expect(browser.getTitle()).toBe('Angular2 Seed');
     });
 
@@ -38,7 +40,6 @@ describe('TODO site', function () {
     it('should delete all notes', function () {
         let notesAll = element.all(by.css('.todo-container [type="checkbox"]'));
 
-        browser.get(URL);
         notesAll.click();
 
         expect(notesAll.count()).toBe(0);
